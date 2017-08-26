9a36cdb75563f70fd224ed32bf04e8b4a58bf0c8
Joel Gilbert <gilbert.279@gmail.com>
2017-08-25 20:38:25 -0500

    Api

---
+++ WeatherApp/WeatherApp/Models/WeatherAppContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WeatherApp.Models
+{
+    public class WeatherAppContext : DbContext
+    {
+        public WeatherAppContext(DbContextOptions<WeatherAppContext> options)
+            :base(options)
+        {
+        }
+
+        public DbSet<WeatherModel> Weather { get; set; }
+    }
+}
